82bc01f672106f1d1048042f27d11fd637c558f6
kangkong <kang3530554@gmail.com>
2022-03-15 18:19:55 +0900

    Start making a To-Do list

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import { HeaderWrapper } from "./Style"
+
+export default function Header() {
+    return (
+        <HeaderWrapper>
+            <div>
+                <h1>To_Do_List</h1>
+            </div>
+        </HeaderWrapper>
+    );
+}
